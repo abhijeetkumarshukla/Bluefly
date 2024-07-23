f010dee0da3187e964f6cb37dcf89718ba174007
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2024-07-24 01:42:19 +0530

    login

--- Backend/controllers/UserRequest/userLogin.controller.js
+++ Backend/controllers/UserRequest/userLogin.controller.js
@@ -1,38 +1,41 @@
-require('dotenv').config();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const UserModel = require('../../models/user.model');
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../../models/user.model");
 
-const userLogin = async(req,res)=>{
-    const {email,password}=req.body;
-    try {
-        const user = await UserModel.findOne({email});
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
 
-        if(!user){
-            return res.status(401).send({message:`wrong Email, Register first`});
-        }bcrypt.compare(password, user.password, async(err,result)=>{
-            if(err){
-                return res
-                .status(500)
-                .send({message: `Error in Comparing password: ${err.massage}`})
-            }
-            if(result){
-                const token = jwt.sign(
-                    {
-                        username:user.username,
-                        email: user.email,
-                        userID:user._id
-                    },
-                    process.env.JWT_SECRET
-                );
-
-                res.status(200).send({message:"Login successful", Token: token});
-
-            }
-        })
-    } catch (error) {
-        res.status(500).send({message:`Wrong Credentials : ${error.message}` });
-
+    if (!user) {
+      return res.status(401).send({ message: "Wrong Email, Register first" });
     }
+
+    bcrypt.compare(password, user.password, async (err, result) => {
+      if (err) {
+        return res
+          .status(500)
+          .send({ message: `Error in Comparing password: ${err.message}` });
+      }
+      if (result) {
+        const token = await jwt.sign(
+          {
+            username: user.username,
+            email: user.email,
+            userID: user._id,
+          },
+          process.env.JWT_SECRET
+        );
+
+        return res.status(200).send({ message: "Login successful", Token: token });
+      } else {
+        return res.status(401).send({ message: "Incorrect password" });
+      }
+    });
+  } catch (error) {
+    return res.status(500).send({ message: `Error: ${error.message}` });
+  }
 };
+
 module.exports = userLogin;

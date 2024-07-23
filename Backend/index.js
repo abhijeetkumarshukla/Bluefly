fa2ca2ac3bff59ea0aa977ac00e69ed3ef8611c8
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2024-07-23 16:21:45 +0530

    login & register

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const connection = require('./config/db')
+const PORT = 8080
+const userRouter = require("./routes/user.routes")
+ 
+const app = express()
+
+app.use(express.json());
+app.use('/user',userRouter);
+
+app.get('/',(req,res)=>{
+    res.status(202).send('Health check done, app is running.')
+})
+
+app.listen(PORT, async()=>{
+    try {
+        await connection
+        console.log(`app is running on port ${PORT} & DB is Connected.`)
+    } catch (error) {
+        console.log(error)
+    }
+})
